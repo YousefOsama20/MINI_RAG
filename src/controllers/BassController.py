5d897308cebf507a35e6c2901e5292266cb62f41
YousefOsama20 <yousefosama7620@gmail.com>
2026-08-03 19:29:46 +0300

    Implement file validation and unique path generation in DataController; add projectController for project path management

--- src/controllers/BassController.py
+++ src/controllers/BassController.py
@@ -1,6 +1,18 @@
 from helpers.config import get_settings, Settings
+import os 
+import random
+import string
 
 class BassController:
 
     def __init__(self):
         self.app_setting = get_settings()
+        
+        self.base_dir = os.path.dirname( os.path.dirname(__file__) )
+        self.files_dir = os.path.join(
+            self.base_dir,
+            "assets/files"
+        )
+
+    def generate_random_string(self, length: int = 8) -> str:
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,5 +1,9 @@
+import os
+from models import ResponseSignal
 from .BassController import BassController
 from fastapi import UploadFile
+from .projectController import projectController
+import re
 
 
 class DataController(BassController):
@@ -11,10 +15,41 @@ class DataController(BassController):
     def  validate_uploaded_file(self, file: UploadFile):
 
         if file.content_type not in self.app_setting.FILE_UPLOAD_TYPES:
-           return False
+           return False , ResponseSignal.FILE_TYPE_NOT_ALLOWED.value
 
         if file.size > self.app_setting.FILE_MAX_SIZE * self.size_scale:
-           return False
-         
-        return True
-        
+           return False , ResponseSignal.FILE_SIZE_EXCEEDED.value
+
+        return True , ResponseSignal.FILE_VALIDATED_SUCCESS.value
+
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str) :
+
+        random_key = self.generate_random_string()
+        project_path = projectController().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name=orig_file_name
+        )
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
+            )
+
+        return new_file_path
+    def get_clean_file_name(self, orig_file_name: str) :
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
