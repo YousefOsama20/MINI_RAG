6131ec4a7264cdaae66833760be9435742d1a4d5
Yousef Osama <yousefosama7620@gmail.com>
2026-09-13 02:12:03 +0300

    fix: correct ID aliases, chunk validation, and typo in database schemas

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -4,11 +4,11 @@ from typing import Optional
 
 class DataChunk(BaseModel):
     
-    _id : Optional[ObjectId]
+    id : Optional[ObjectId] = Field(None, alias ="_id")
     chunk_text : str = Field(..., min_length=1)
     chunk_metadata:  dict   
-    chunk_order: int = Field(..., min_length=1)
-    chnk_projcet_id: ObjectId
+    chunk_order: int = Field(..., gt=0)
+    chunk_project_id: ObjectId
 
     class Config:
         arbitrary_types_allowed = True    
