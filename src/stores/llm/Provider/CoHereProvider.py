0181b61b8b751efc491e698ccf79d8e157a885ae
Yousef Osama <yousefosama7620@gmail.com>
2026-09-16 12:41:48 +0300

    cteate CoHere Provider

---
+++ src/stores/llm/Provider/CoHereProvider.py
@@ -0,0 +1,27 @@
+from ..LLMInterface import LLMInterface
+from ..LLMEnums import CohereEnum
+import cohere
+import logging
+
+class CoHereProvider(LLMInterface):
+    
+    def __init__(self, api_key: str, api_url: str=None,
+                       default_input_max_characters: int=1000,
+                       default_generation_max_output_tokens: int=1000,
+                       default_generation_temperature: float=0.1):
+        
+        self.api_key = api_key
+
+        self.default_input_max_characters = default_input_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
+        self.default_generation_temperature = default_generation_temperature
+
+        self.generation_model_id = None
+
+        self.embedding_model_id = None
+        self.embedding_size = None
+
+        self.client = cohere.Client(  
+            api_key = self.api_key
+        )
+        self.logger = logging.getLogger(__name__)
